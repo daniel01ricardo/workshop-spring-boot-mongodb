e188d6cd2a9fd8353175f8d97b7ecfea68276e37
Daniel01ricardo <daniel01ricardo@hotmail.com>
2024-11-09 07:43:07 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/daniel01ricardo/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel01ricardo.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
